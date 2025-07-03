f01a9e0bf4b6f4e0c429758e418e395992c954a5
Sheape <86521166+Sheape@users.noreply.github.com>
2025-07-03 21:30:35 +0800

    chore: added debug print for parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,10 @@
 use crate::{
     lexer::{Token, TokenType},
-    program::Declaration,
+    program::{Declaration, AST},
     Result,
 };
 use crate::{program::ASTNode, Error};
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 #[derive(Debug)]
 pub struct Parser<'a> {
@@ -38,7 +38,6 @@ pub enum Statement {
 
 #[derive(Debug)]
 pub enum ExprNodeType {
-    Root,
     Binary,
     Unary,
     Grouping,
@@ -86,8 +85,6 @@ impl<'a> Parser<'a> {
     }
 
     fn read_token(&mut self) {
-        //let current_token_idx = self.current_token_idx.get();
-        //self.current_token_idx.set(current_token_idx + 1);
         self.current_token_idx += 1;
     }
 
@@ -114,14 +111,16 @@ impl<'a> Parser<'a> {
         }
     }
 
-    fn parse_statement(&mut self) -> Result<Statement> {
-        match self.get_token_type() {
+    fn parse_statement(&mut self) -> Result<NodeId> {
+        let statement = match self.get_token_type() {
             Some(TokenType::PRINT) => self.parse_print_statement().map(Statement::PrintStatement),
             //_ => self.parse_expr_statement(),
             _ => todo!(),
-        }
+        };
+
+        self.ast.push(ASTNode::Statement(statement?));
+        Ok(NodeId(self.ast.len() - 1))
     }
-    //
 
     fn parse_print_statement(&mut self) -> Result<NodeId> {
         self.read_token(); // Skip "print"
@@ -215,7 +214,7 @@ impl<'a> Parser<'a> {
 
         Ok(factor)
     }
-    //
+
     fn parse_factor(&mut self) -> Result<NodeId> {
         let mut unary = self.parse_unary()?;
         while let Some(current_token) = self.get_token() {
@@ -239,7 +238,7 @@ impl<'a> Parser<'a> {
 
         Ok(unary)
     }
-    //
+
     fn parse_unary(&mut self) -> Result<NodeId> {
         match self.get_token() {
             Some(token) if matches!(token.token_type, TokenType::BANG | TokenType::MINUS) => {
@@ -264,13 +263,13 @@ impl<'a> Parser<'a> {
         let expression = match current_token.token_type {
             TokenType::LEFT_PAREN => {
                 let node = self.parse_expr()?;
-                self.read_token();
                 //let closing_paren = self.get_token();
                 match self.get_token() {
                     Some(closing_paren) if closing_paren.token_type == TokenType::RIGHT_PAREN => {
+                        self.read_token();
                         Ok(Expression {
                             node_type: ExprNodeType::Grouping,
-                            main_token: closing_paren,
+                            main_token: current_token,
                             lhs: Some(node),
                             rhs: None,
                         })
@@ -300,5 +299,74 @@ impl<'a> Parser<'a> {
 
         self.ast.push(ASTNode::Expression(expression?));
         Ok(NodeId(self.ast.len() - 1))
+    }
+}
+
+//impl Display for Vec<ASTNode<'_>> {
+//    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//        todo!()
+//    }
+//}
+
+impl<'a> AST<'a> {
+    // We implement our own display method instead of impl Display because we still need access to AST.
+    fn display(&self, node: &ASTNode<'a>) -> String {
+        match node {
+            ASTNode::Declaration(declaration) => match declaration {
+                Declaration::Class {
+                    name,
+                    inherited_class,
+                    methods,
+                } => todo!(),
+                Declaration::Function(function) => todo!(),
+                Declaration::Variable { name, expression } => todo!(),
+                Declaration::Statement(statement) => self.display(&self.0[statement.0]),
+            },
+            ASTNode::Statement(statement) => match statement {
+                Statement::ExprStatement(node_id) => {
+                    format!("(expression {})", self.display(&self.0[node_id.0]))
+                }
+                Statement::PrintStatement(node_id) => {
+                    format!("(print {})", self.display(&self.0[node_id.0]))
+                }
+            },
+            ASTNode::Expression(expression) => match expression.node_type {
+                ExprNodeType::Binary => {
+                    // Its okay to clone here because we're just displaying the parsed debug info.
+                    let lhs = self.display(&self.0[expression.lhs.clone().unwrap().0]);
+                    let rhs = self.display(&self.0[expression.rhs.clone().unwrap().0]);
+                    format!("({} {lhs} {rhs})", expression.main_token.token_type)
+                }
+                ExprNodeType::Unary => {
+                    let unary = self.display(&self.0[expression.lhs.clone().unwrap().0]);
+                    format!("({} {unary})", expression.main_token.token_type)
+                }
+                ExprNodeType::Grouping => format!(
+                    "(group {})",
+                    self.display(&self.0[expression.lhs.clone().unwrap().0])
+                ),
+                ExprNodeType::Literal => format!("{}", expression.main_token.token_type),
+            },
+        }
+    }
+}
+
+impl Display for AST<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ast = &self.0;
+
+        let parsed = ast
+            .iter()
+            .filter_map(|node| {
+                if let ASTNode::Declaration(_) = node {
+                    Some(self.display(node))
+                } else {
+                    None
+                }
+            })
+            .reduce(|acc, e| format!("{acc}\n{e}"))
+            .unwrap_or("".to_owned());
+
+        write!(f, "{parsed}")
     }
 }
